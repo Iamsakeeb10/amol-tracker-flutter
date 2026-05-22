5ae288b25ad110c3bf10eedc08063db611984da6
Shakib Ahmed <shakibshovon.10@gmail.com>
2026-05-22 23:54:45 +0600

    added split api apk block in build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -59,6 +59,15 @@ android {
         }
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     dependencies {
         coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     }

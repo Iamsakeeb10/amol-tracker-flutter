fe89a3ab1215ab0e2190f563569ca1e1599b2f79
Shakib Ahmed <shakibshovon.10@gmail.com>
2026-06-25 01:53:48 +0600

    added more items into quick navigation

--- lib/features/home/presentation/widgets/home_quick_nav_section.dart
+++ lib/features/home/presentation/widgets/home_quick_nav_section.dart
@@ -17,6 +17,62 @@ class HomeQuickNavSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
 
+    final items = [
+      _QuickNavCard(
+        icon: Icons.auto_stories_outlined,
+        label: l10n.quranTitle,
+        onTap: () => context.push(AppRoutes.quran),
+      ),
+      _QuickNavCard(
+        icon: Icons.menu_book_outlined,
+        label: l10n.navDua,
+        onTap: () => context.push(AppRoutes.dua),
+      ),
+      _QuickNavCard(
+        icon: Icons.wb_sunny_outlined,
+        label: l10n.morningEveningDua,
+        onTap: () => context.push(
+          AppRoutes.dua,
+          extra: _morningEveningCategoryUrl,
+        ),
+      ),
+      _QuickNavCard(
+        icon: Icons.emoji_events_outlined,
+        label: l10n.leaderboard,
+        onTap: () => context.push(AppRoutes.leaderboard),
+      ),
+      _QuickNavCard(
+        icon: Icons.calendar_month_outlined,
+        label: l10n.hijriCalendar,
+        onTap: () => context.push(AppRoutes.hijriCalendar),
+      ),
+      _QuickNavCard(
+        icon: Icons.explore_outlined,
+        label: l10n.qiblaTitle,
+        onTap: () => context.push(AppRoutes.qibla),
+      ),
+      _QuickNavCard(
+        icon: Icons.touch_app_outlined,
+        label: l10n.dhikrCounter,
+        onTap: () => context.push(AppRoutes.dhikr),
+      ),
+      _QuickNavCard(
+        icon: Icons.star_outline,
+        label: l10n.asmaUlHusna,
+        onTap: () => context.push(AppRoutes.asmaUlHusna),
+      ),
+      _QuickNavCard(
+        icon: Icons.school_outlined,
+        label: l10n.syllabusTitle,
+        onTap: () => context.push(AppRoutes.syllabus),
+      ),
+      _QuickNavCard(
+        icon: Icons.notifications_active_outlined,
+        label: l10n.prayerAdhanScreenTitle,
+        onTap: () => context.push(AppRoutes.prayerAdhan),
+      ),
+    ];
+
     return SliverToBoxAdapter(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -25,39 +81,28 @@ class HomeQuickNavSection extends StatelessWidget {
             l10n.quickNavSection,
             style: AppTextStyles.headlineMedium(context),
           ),
-
           SizedBox(height: 8.h),
-
-          Row(
-            children: [
-              Expanded(
-                child: _QuickNavCard(
-                  icon: Icons.auto_stories_outlined,
-                  label: l10n.quranTitle,
-                  onTap: () => context.push(AppRoutes.quran),
+          for (int i = 0; i < items.length; i += 3) ...[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(child: items[i]),
+                SizedBox(width: 10.w),
+                Expanded(
+                  child: i + 1 < items.length
+                      ? items[i + 1]
+                      : const SizedBox.shrink(),
                 ),
-              ),
-              SizedBox(width: 10.w),
-              Expanded(
-                child: _QuickNavCard(
-                  icon: Icons.menu_book_outlined,
-                  label: l10n.navDua,
-                  onTap: () => context.push(AppRoutes.dua),
+                SizedBox(width: 10.w),
+                Expanded(
+                  child: i + 2 < items.length
+                      ? items[i + 2]
+                      : const SizedBox.shrink(),
                 ),
-              ),
-              SizedBox(width: 10.w),
-              Expanded(
-                child: _QuickNavCard(
-                  icon: Icons.wb_sunny_outlined,
-                  label: l10n.morningEveningDua,
-                  onTap: () => context.push(
-                    AppRoutes.dua,
-                    extra: _morningEveningCategoryUrl,
-                  ),
-                ),
-              ),
-            ],
-          ),
+              ],
+            ),
+            if (i + 3 < items.length) SizedBox(height: 10.h),
+          ],
         ],
       ),
     );
